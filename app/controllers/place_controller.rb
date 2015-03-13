21266f8e249f9c218cc456530e432d5d9bcb7b46
Aaron Jones <saasbook@saasbook.(none)>
2015-03-13 15:30:54 -0300

    Update place controller

--- app/controllers/place_controller.rb
+++ app/controllers/place_controller.rb
@@ -6,8 +6,20 @@ class PlaceController < ApplicationController
 	end
 
 	def index
-		@selectedPlace
+		
 	end
+
+
+  def create
+    @place=Place.create!(place_params)
+    flash[:notice] = "#{@place.name} was successfully created."
+  end
+
+  def destroy
+    @place = Place.find(params[:placeid])
+    @place.destroy
+    flash[:notice] = "#{@place.name} was deleted"
+  end
 
 	def place_params
 		params.require(:place).permit(:placeid, :name, :type, :price, :popularity)
