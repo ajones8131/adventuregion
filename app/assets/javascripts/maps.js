4f98aa79bfc08c79e33e89a15e1195c882efa4a1
abailes <abailes@uccs.edu>
2015-03-13 03:39:00 -0300

    Added a bunch of Places API

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -2,6 +2,7 @@
 	var UCCSLat = 38.893659;
 	var UCCSLng = -104.800321;
 
+	// Variables for various functions
 	var map;
 	var input;
 
@@ -12,6 +13,9 @@
 	var directionsDisplay;
 	var directionsService = new google.maps.DirectionsService();
 
+	// Variable for Predictions
+	var predictService = new google.maps.places.AutocompleteService();
+
 	// Layers can only be used one at a time unless using a fusion table.
 	var setTrafficLayer = false;
 	var setBicycleLayer = false;
@@ -19,12 +23,15 @@
 	// Initializes the map canvas
 	function initialize() {
 
+		var mapOptions = {
+			center: new google.maps.LatLng(UCCSLat, UCCSLng),
+			zoom: 13,
+			mapTypeID: google.maps.MapTypeId.ROADMAP
+		};
+
 	    directionsDisplay = new google.maps.DirectionsRenderer(); 	
 
-	    map = new google.maps.Map(document.getElementById('map-canvas'), {
-	    	zoom: 7,
-	        mapTypeID: google.maps.MapTypeId.ROADMAP
-	    });
+	    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
 	    directionsDisplay.setMap(map);
 
@@ -32,12 +39,11 @@
 	        new google.maps.LatLng(UCCSLat, UCCSLng)
 	    );
 
-	    map.fitBounds(defaultBounds)
+	    map.fitBounds(defaultBounds);
 
 	    var request = {
 	        location: defaultBounds,
 	        radius: 500,
-	        types: ['parks']
 	    }
 
 	    var markers = [];
@@ -51,15 +57,42 @@
 	    google.maps.event.addListener(searchBox, 'places_changed', function() {
 	        var places = searchBox.getPlaces();
 
-	        // Create a marker for each place.
-	        createMarker(places)
+	        if (places.length == 0) {
+      			return;
+    		}
+    		for (var i = 0, marker; marker = markers[i]; i++) {
+      			marker.setMap(null);
+    		}
 
+    		markers = [];
+    		var bounds = new google.maps.LatLngBounds();
+    		for (var i = 0, place; place = places[i]; i++) {
+      			var image = {
+			        url: place.icon,
+			        size: new google.maps.Size(71, 71),
+			        origin: new google.maps.Point(0, 0),
+			        anchor: new google.maps.Point(17, 34),
+			        scaledSize: new google.maps.Size(25, 25)
+		      	};
+
+	      	var marker = new google.maps.Marker({
+	      		map: map,
+	      		icon: image,
+	      		title: place.name,
+	      		position: place.geometry.location
+	      	});
+
+	      	markers.push(marker);
 	        bounds.extend(place.geometry.location);
+			}
+
+	    	map.fitBounds(bounds);
 	    });
 
 	    infowindow = new google.maps.InfoWindow();
 	    var service = new google.maps.places.PlacesService(map);
-	    service.nearbySearch(request, callback);
+	    service.nearbySearch(request, callbackPlaces);
+	    predictService.getQueryPredictions({input: input}, callbackPredictions);
 
 	    // Bias the SearchBox results towards places that are within the bounds of the
 	    // current map's viewport.
@@ -69,29 +102,45 @@
 	    });
 
 	    directionsDisplay.setMap(map);
-
-	    // Set different layers on top of Map canvas
-	    setLayers();
 	}
 
-	// Calls back with results and status of APIs
-	function callback(results, status) {
-	    if (status == google.maps.places.PlacesServiceStatus.OK) {
-	        for (var i = 0; i < results.length; i++) {
-	            createMarkers(results[i]);
-	        }
+	// Calls back with results and status of Places API
+	function callbackPlaces(results, status) {
+	  if (status != google.maps.places.PlacesServiceStatus.OK) {
+	  	alert(status);
+	  	return;
+	  }
+	  else {
+	    for (var i = 0; i < results.length; i++) {
+	      createMarker(results[i]);
 	    }
+	  }
 	}
 
-	// Calculate the route
+	// Calls back with predictions and status of Prediction API
+	function callbackPredictions(predictions, status) {
+		if ( status != google.maps.places.PlacesServiceStatus.OK) {
+			alert(status);
+			return;
+		}
+
+		var results = document.getElementById('results');
+
+		for (var i = 0, prediction; prediction = prediction[i]; i++)	{
+			results.innerHTML += '<li>' + prediction.description + '</li>';
+		}
+	}
+
+	// Calculate the route from start location to end location
 	function calcRoute() {
 		var start = document.getElementById('start').value;
 	  	var end = document.getElementById('end').value;
+	  	var selectMode = document.getElementById('selectMode').value;
 
 		var request = {
 			origin: start,
 			destination: end,
-			travelMode: google.maps.TravelMode.DRIVING
+			travelMode: google.maps.TravelMode[selectMode]
 		};
 
 		directionsService.route(request, function(response, status) {
@@ -99,33 +148,6 @@
 		      directionsDisplay.setDirections(response);
 		    }
 	  	});
-	}
-
-	// Creates markers for places
-	function createMarker(place) {
-	    var placeLoc = place.geometry.location;
-	    var marker = new google.maps.Marker({
-	        map: map,
-	        position: place.geometry.location
-	    });
-
-	    google.maps.event.addListener(marker, 'click', function() {
-	        infowindow.setContent(place.name);
-	        infowindow.open(map, this);
-	    });
-	}
-
-	// Handle Layers for Map canvas
-	function setLayers() {
-	    if (setTrafficLayer) {
-	        var trafficLayer = new google.maps.TrafficLayer();
-	        trafficLayer.setMap(map);
-	    }
-
-	    if (setBicycleLayer) {
-	        var bicycleLayer = new google.maps.BicyclingLayer();
-	        bicycleLayer.setMap(map);
-	    }
 	}
 
 	// Adds listener and calls the initialize
