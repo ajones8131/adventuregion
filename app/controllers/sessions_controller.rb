c612250928cd6b5aeae3c91081dcb069702fd116
Andrew Bailes <abailes@uccs.edu>
2015-04-03 20:27:22 -0600

    Stuff

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,22 @@
 class SessionsController < ApplicationController
-  def new
-  end
+  # def new
+  # end
 
-  def create
-  	user = User.find_by_email(params[:sessions][:email].downcase)
-  	if user && user.authenticate(params[:sessions][:password])
-  		log_in user
-  		params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-  		remember user
-  		redirect_to user_path
-  	else
-  		flash[:danger] = 'Invalid Email/Password combination'
-  		render 'new'
-  	end
-  end
+  # def create
+  # 	user = User.find_by_email(params[:sessions][:email].downcase)
+  # 	if user && user.authenticate(params[:sessions][:password])
+  # 		log_in user
+  # 		params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+  # 		remember user
+  # 		redirect_to user_path
+  # 	else
+  # 		flash[:danger] = 'Invalid Email/Password combination'
+  # 		render 'new'
+  # 	end
+  # end
 
-  def destroy
-  	log_out if logged_in?
-  	redirect_to root_url
-  end
+  # def destroy
+  # 	log_out if logged_in?
+  # 	redirect_to root_url
+  # end
 end
