219db0f11f104709203bed06e5784e0f74ba892e
Aaron Jones <ajones17@uccs.edu>
2015-04-03 22:19:22 -0300

    Spec tests for place controller

--- spec/models/add_place_spec.rb
+++ spec/models/add_place_spec.rb
@@ -6,7 +6,7 @@ feature 'Add new Place' do
     visit '/place/index'
     click_link "Add new place"
   end
-  scenario 'Create new place' do
+  scenario 'Create new place and Show page' do
     fill_in 'place_name', with: "Pikes Peak"
     fill_in 'place_placeid', with:	"1000"
     fill_in 'place_category', with: "Hiking"
@@ -14,5 +14,21 @@ feature 'Add new Place' do
     fill_in 'place_popularity', with: "medium"
     click_button "Create Place"
     expect(page).to have_content("Pikes Peak")
+    click_link "Pikes Peak"
+    expect(page).to have_content("Showing Details for Pikes Peak")
+    expect(page).to have_content("Place ID = 1000")
+    expect(page).to have_content("Category = Hiking")
+    expect(page).to have_content("Price = low")
+    expect(page).to have_content("Popularity = medium")
+    expect(page).to have_content("Showing Details for Pikes Peak")
+  end
+end
+
+feature 'Edit Place' do
+  before do
+    @place = Place.create(name:"Pikes Peak", placeid:"1000", category:"Hiking", price:"low", popularity:"medium")
+  end
+  scenario 'Edit Pikes Peak' do
+    
   end
 end
