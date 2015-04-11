d367aa4718b0776e8a3566b27e6f4a54fb6fd98a
Andrew Bailes <abailes@uccs.edu>
2015-04-10 20:40:47 -0600

    Fixed another merge error

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,24 +1,17 @@
 module SessionsHelper
 
 	def sign_in(user)
-		session[:user_id] = user.id
 	end
 
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
+	def current_user
+	end
 
-  def signed_in?
-    current_user.nil?
-  end
+	def signed_in?
+	end
 
 	def sign_out
-    session.delete(:user_id)
-		@current_user = nil
 	end
 
 	def redirect_back_to( path )
-    	redirect_to(session[:return_to] || path)
-    	session.delete(:return_to)
 	end
 end
