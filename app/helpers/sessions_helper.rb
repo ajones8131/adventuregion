5cb359c8a676583d856ba8521ee60b2ebefba75b
Andrew Bailes <abailes@uccs.edu>
2015-04-10 20:15:50 -0600

    Some Signing in Changes

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,15 +1,16 @@
 module SessionsHelper
+
 	def sign_in(user)
-		self.current_user = user
+		session[:user_id] = user.id
 	end
 
-	def signed_in?
-  	!current_user.nil?
-	end
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 
-	def current_user=(user)
-  	@current_user ||= User.find_by(id: session[:user_id])
-	end
+  def signed_in?
+    current_user.nil?
+  end
 
 	def sign_out
     session.delete(:user_id)
