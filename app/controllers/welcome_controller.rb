517e02d40b3a9d4ee0ac48240d17d625a2ce7b97
Adam Barker <abarker2@uccs.edu>
2015-04-17 21:46:55 -0300

    fixed everything that was broken from last merge, uncommented random function.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
-  # helper_method :get_random
+  helper_method :get_random
   def index
   	ordering, @type_header = {:name => :asc}, 'hilite'
     @places = Place.order(ordering)
@@ -8,14 +8,8 @@ class WelcomeController < ApplicationController
   def place
   end
 
-  # def get_random
-  #   id = rand(1000000) % Place.all.length
-  #   return Place.find(id + 1)
-  # end
-
-  # def get_random
-  #   r = Random.new()
-  #   r.rand(Place.all.count) + 1
-  #   return Place.find_by(:id => r)
-  # end
+  def get_random
+    id = rand(Place.all.length)
+    return Place.find(id + 1)
+  end
 end
