1cd8922c7a9b4545f563b8fcc610bef4563c4d9a
Andrew Bailes <abailes@uccs.edu>
2015-04-24 19:25:03 -0600

    Some Refactoring

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,15 +7,8 @@ class WelcomeController < ApplicationController
   def index
   	ordering, @type_header = {:name => :asc}, 'hilite'
     @places = Place.order(ordering)
-
-    #@random = get_random
   end
   
   def place
-  end
-
-  def get_random
-    id = rand(Place.all.length).round
-    return Place.where(id + 1)
   end
 end
